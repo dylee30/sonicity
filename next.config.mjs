3b0aa6c2fe8695ca542f0fef5eaea76a5768b8a7
dylee30 <davidyklee30@gmail.com>
2024-05-23 23:56:57 -0400

    final

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "yycyumtrgyqbpwrakeop.supabase.co"
+        ]
+    }
+};
+
+export default nextConfig;
